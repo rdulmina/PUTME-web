707fb8f446c03b8bfda302a5a9a39c6365988aaf
Unknown <wasuradananjith@gmail.com>
2017-12-20 13:10:36 +0530

    logo image added

--- putmedb.sql
+++ putmedb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2017 at 03:29 PM
--- Server version: 5.7.9
--- PHP Version: 5.6.16
+-- Generation Time: Dec 20, 2017 at 07:08 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,8 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `bin`
 --
 
-DROP TABLE IF EXISTS `bin`;
-CREATE TABLE IF NOT EXISTS `bin` (
+CREATE TABLE `bin` (
   `bin_id` varchar(10) NOT NULL,
   `location` varchar(50) NOT NULL,
   `description` varchar(50) NOT NULL
@@ -36,23 +35,107 @@ CREATE TABLE IF NOT EXISTS `bin` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `driver`
+--
+
+CREATE TABLE `driver` (
+  `driverid` int(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `phoneno` varchar(10) NOT NULL,
+  `is_assigned` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `driver_location`
+--
+
+CREATE TABLE `driver_location` (
+  `driverid` int(10) NOT NULL,
+  `area` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `filled_request`
 --
 
-DROP TABLE IF EXISTS `filled_request`;
-CREATE TABLE IF NOT EXISTS `filled_request` (
-  `req_id` varchar(10) NOT NULL,
+CREATE TABLE `filled_request` (
+  `req_id` int(10) NOT NULL,
   `bin_id` varchar(10) NOT NULL,
-  PRIMARY KEY (`req_id`)
+  `states` tinyint(2) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `filled_request`
 --
 
-INSERT INTO `filled_request` (`req_id`, `bin_id`) VALUES
-('REQ000001', 'BIN1');
+INSERT INTO `filled_request` (`req_id`, `bin_id`, `states`) VALUES
+(1, 'BIN1', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userid` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bin`
+--
+ALTER TABLE `bin`
+  ADD PRIMARY KEY (`bin_id`);
+
+--
+-- Indexes for table `driver`
+--
+ALTER TABLE `driver`
+  ADD PRIMARY KEY (`driverid`);
+
+--
+-- Indexes for table `driver_location`
+--
+ALTER TABLE `driver_location`
+  ADD PRIMARY KEY (`driverid`);
+
+--
+-- Indexes for table `filled_request`
+--
+ALTER TABLE `filled_request`
+  ADD PRIMARY KEY (`req_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `driver`
+--
+ALTER TABLE `driver`
+  MODIFY `driverid` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `filled_request`
+--
+ALTER TABLE `filled_request`
+  MODIFY `req_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
